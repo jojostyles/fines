504b0d6349804c033e0d0534427e0ded49d11efb
joey-trip <au.joseph.tripodi@gmail.com>
2020-02-21 19:49:50 +1030

    Adds models following the diagram in issue
    Contributes to issue #15

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb+srv://admin:DatabaseIsSafe69@finecluster-fpu6t.mongodb.net/development?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
